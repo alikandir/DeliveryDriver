52b1d01c61cc9bb7f1453e3760956abf8d03b349
alikandir <alikandir@gmail.com>
2023-03-25 00:50:55 +0300

    PlayableFirstVersion

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+  [SerializeField] Color32 hasPackageColor = new Color32 (1,1,1,1); 
+  [SerializeField] Color32 noPackageColor = new Color32 (0,0,0,0);
+  [SerializeField] float destroyDelay = 0.5f;
+  
+  SpriteRenderer spriteRenderer;
+  
+  void Start()
+  {
+    spriteRenderer = GetComponent<SpriteRenderer>();      
+  }
+    bool hasPackage;
+    void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.tag == "Package" && !hasPackage)
+    {
+     Debug.Log("Package picked up");
+     hasPackage = true;
+     Destroy(other.gameObject, destroyDelay); 
+     spriteRenderer.color = hasPackageColor;
+    }
+    
+    if (other.tag =="Customer" && hasPackage)
+    {
+      Debug.Log("Package delivered!");
+      hasPackage = false;
+      spriteRenderer.color = noPackageColor;
+    }
+  }
+  
+}
+
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -5,23 +5,46 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
   [SerializeField] float steerSpeed = 0.1f;
-  [SerializeField] float moveSpeed = 0.1f;
+  [SerializeField] float moveSpeed = 20f;
+  [SerializeField] float slowSpeed = 15f;
+  [SerializeField] float boostSpeed = 25f;
        void Start()
-    {
+  {
     
-    }
+  }
 
       void Update()
+  {
+      float steerAmount = Input.GetAxisRaw("Horizontal") * steerSpeed*Time.deltaTime;
+      float moveAmount = Input.GetAxisRaw("Vertical")*moveSpeed*Time.deltaTime;
+      
+      if (moveAmount>=0)
     {
-        float steerAmount = Input.GetAxisRaw("Horizontal") * steerSpeed*Time.deltaTime;
-        float moveAmount = Input.GetAxisRaw("Vertical")*moveSpeed*Time.deltaTime;
-                if (moveAmount>=0){
             transform.Rotate(0, 0, -steerAmount);
             transform.Translate(0, moveAmount,0);
-          }
-          else{
+    }
+
+      else
+    {
             transform.Rotate(0, 0, steerAmount);
             transform.Translate(0, moveAmount,0);
-          }
     }
+  }
+    
+    void OnTriggerEnter2D(Collider2D other)
+  {
+      if (other.tag == "Boost")
+    {
+      moveSpeed = boostSpeed;
+    }
+    
+  }
+ 
+  void OnCollisionEnter2D(Collision2D other)
+  {
+      if (other.gameObject.tag == "Obstacle")
+    {
+      moveSpeed = slowSpeed;
+    }
+  }
 }
